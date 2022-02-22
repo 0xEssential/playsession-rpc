3f79230594c3f68f405c0cd484f279de5af63e3a
Sam Bauch <sbauch@gmail.com>
2022-02-21 20:59:37 -0500

    no localhost

--- server.ts
+++ server.ts
@@ -114,4 +114,4 @@ async function durinCall({ callData, to, abi: _abi }, _opt, callback) {
 }
 
 server.expose('durin_call', durinCall);
-server.listen(process.env.PORT, 'localhost');
+server.listen(process.env.PORT);
